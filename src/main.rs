0a1b8e30215f7294f189a4680ebc8582ea7e802a
asm <ahmed.assem@flairstech.com>
2024-04-28 00:20:46 +0300

    Refactor audio tests to use synchronous methods instead of async

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn main() -> Result<()> {
 
     //tests::test_python()?;
 
-    tests::test_sound().await?;
+    //tests::test_sound().await?;
     //tests::test_image().await?;
 
     //tests::test_slog()?;
