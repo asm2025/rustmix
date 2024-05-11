e5017e51e7c5f31cebc027ab16330411fcaae5e9
asm <ahmed.assem@flairstech.com>
2024-05-11 04:33:39 +0300

    Refactor log4rs configuration to use path join instead of manual string concatenation

--- src/tests/log4rs.rs
+++ src/tests/log4rs.rs
@@ -1,10 +1,7 @@
 use log::{debug, error, info, trace, warn};
 
 use rustmix::{
-    io::{
-        directory,
-        path::{AsPath, PathEx},
-    },
+    io::{directory, path::AsPath},
     log4rs, Result,
 };
 
@@ -12,26 +9,20 @@ pub fn test_log4rs(from_config_file: bool) -> Result<()> {
     println!("\nTesting log4rs functions...");
 
     if from_config_file {
-        println!("Building loggers from file...");
-        let path = (
-            directory::current().as_str(),
-            "..",
-            "files",
-            "log",
-            "log4rs.yaml",
-        )
-            .as_full_path();
+        let path = directory::current().join("files/log/log4rs.yaml");
+        println!("Building loggers from file: {}", &path.display());
+        println!("file exist? {}", &path.exists());
         log4rs::from_file(&path)?;
         println!("Logger was built");
         log_a_few_messages("Messages configured logger from a yaml file:");
-        println!("Check the log file at: {}", &path);
+        println!("Check the log file: {}", &path.display());
     } else {
-        println!("Building loggers from code...");
         let path = ("_logs", "test.log").as_path();
+        println!("Building loggers from code: {}", &path);
         log4rs::build(&path)?;
         println!("Logger was built");
         log_a_few_messages("Messages configured logger programmatically:");
-        println!("Check the log file at: {}", &path);
+        println!("Check the log file: {}", &path);
     }
 
     Ok(())
