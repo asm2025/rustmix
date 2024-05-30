4d73e5f70bfcdcddc8e2535107b38a31125f4735
asm <ahmed.assem@flairstech.com>
2024-05-30 17:33:39 +0300

    Update Kalosm

--- models/rwhisper/src/lib.rs
+++ models/rwhisper/src/lib.rs
@@ -44,7 +44,7 @@ use model::WhisperInner;
 use rodio::{source::UniformSourceIterator, Source};
 use std::{fmt::Display, str::FromStr, time::Duration};
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 
 use candle_transformers::models::whisper::{self as m};
 
@@ -319,22 +319,13 @@ impl WhisperBuilder {
             .await?;
 
         let (rx, tx) = std::sync::mpsc::channel();
-        let (erx, etx) = std::sync::mpsc::channel();
         let thread = std::thread::spawn(move || {
             tokio::runtime::Builder::new_current_thread()
                 .build()
                 .unwrap()
                 .block_on(async move {
-                    let model = WhisperInner::new(self, filename, tokenizer_filename, config);
-
-                    if model.is_err() {
-                        let err = model.err().unwrap();
-                        erx.send(err).unwrap();
-                        return;
-                    }
-
-                    let mut model = model.unwrap();
-
+                    let mut model =
+                        WhisperInner::new(self, filename, tokenizer_filename, config).unwrap();
                     while let Ok(message) = tx.recv() {
                         match message {
                             WhisperMessage::Kill => return,
@@ -346,14 +337,10 @@ impl WhisperBuilder {
                 });
         });
 
-        match etx.try_recv() {
-            Ok(err) => Err(err.into()),
-            Err(std::sync::mpsc::TryRecvError::Empty) => Ok(Whisper {
+        Ok(Whisper {
                 thread: Some(thread),
                 sender: rx,
-            }),
-            Err(std::sync::mpsc::TryRecvError::Disconnected) => Err(anyhow!("thread panicked")),
-        }
+        })
     }
 
     /// Set the model to be used.
